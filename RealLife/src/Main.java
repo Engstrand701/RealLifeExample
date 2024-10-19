540bed343300123122e9e65e51b8ea9ff376a795
Jonathan Engstrand <141049701+Engstrand701@users.noreply.github.com>
2024-10-19 13:58:56 +0100

    Add files via upload
    I want to get to where the user can input data and get the solutions.

---
+++ RealLife/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        // Creating variables of different types.
+        int items = 64;
+        float costPerItem = 9.99f;
+        float totalCost = costPerItem * items;
+        char currency ='£';
+
+        // Printing off the variables.
+
+        System.out.println("Number of items:" + items);
+        System.out.println("Cost per item:" + costPerItem);
+        System.out.println("Total cost:" + totalCost);
+
+    }
+}
